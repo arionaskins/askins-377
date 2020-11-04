5bc7503f197ab773008f0f73f91fdf9d435a6ebb
juanerick97 <70673871+juanerick97@users.noreply.github.com>
2020-11-03 23:38:41 -0500

    lab 7 semifinal commit
    - fixes unique methoid
    - fixes resturant list
    - adds brackets and labels to chart

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -1,6 +1,28 @@
+
+
 function convertRestaurantsToCategories(restaurantList) {
   // process your restaurants here!
-  return list;
+  var categoryArr = restaurantList.map(restaurantList => restaurantList.category);
+  const categories = uniqueCategories(categoryArr);
+  var restaurantToCategoryList = [];
+  var categoryCount = 0;
+  var k = 0;
+
+  console.log("CategoriesFunction", uniqueCategories(categoryArr));
+
+  for(var i = 0; i < categories.length; i++){ 
+    for(var j = 0; j < categoryArr.length; j++){
+      if(categories[i] == categoryArr[j]){ //Counts the number of unique categories in all the categories given by jsonfromserver
+        categoryCount++;
+      }
+    }
+
+    restaurantToCategoryList[k++] = {y: categoryCount, label: categories[i]}; //Adds count and category to list
+    categoryCount = 0;
+  }
+
+
+  return restaurantToCategoryList;
 }
 
  function uniqueCategories(categoriesArr){
@@ -9,11 +31,12 @@ function convertRestaurantsToCategories(restaurantList) {
   var i = 0;
   var j = 0;
  
-   for(i = 0; i < categoriesArr.length; i++){
+  for(i = 0; i < categoriesArr.length; i++){
     tempCategory = categoriesArr[i];
-   
-    if(uniqueCategoryArr.includes(tempCategory) == false)
+
+    if(uniqueCategoryArr.includes(tempCategory) == false){
       uniqueCategoryArr[j++] = tempCategory;
+    }
   }
 
   return uniqueCategoryArr;
@@ -29,7 +52,7 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
     animationEnabled: true,
     colorSet: 'customColorSet1',
     title: {
-      text: 'Change This Title'
+      text: 'Places To Eat Out In Future'
     },
     axisX: {
       interval: 1,
@@ -37,10 +60,13 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
     },
     axisY2: {
       interlacedColor: 'rgba(1,77,101,.2)',
-      gridColor: 'rgba(1,77,101,.1)',
-      title: 'Change This Title',
+      gridColor: 'rgb(1,77,101,.1)',
+      title: 'Resturants by Category',
       labelFontSize: 12,
-      scaleBreaks: {customBreaks: []} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+      scaleBreaks: {customBreaks: [
+        {startValue: 40,  endValue: 50, color: "white"}, 
+        {startValue: 60,  endValue: 100, color: "white"}, 
+        {startValue: 110,  endValue: 200, color: "white"}]} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
     },
     data: [{
       type: 'bar',
@@ -52,12 +78,15 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
 }
 
 function runThisWithResultsFromServer(jsonFromServer) {
+  //console.log("categoryTest", uniqueCategories(test))
+
   console.log('jsonFromServer', jsonFromServer);
   sessionStorage.setItem('restaurantList', JSON.stringify(jsonFromServer)); // don't mess with this, we need it to provide unit testing support
   // Process your restaurants list
   // Make a configuration object for your chart
   // Instantiate your chart
   const reorganizedData = convertRestaurantsToCategories(jsonFromServer);
+  console.log("List", convertRestaurantsToCategories(jsonFromServer));
   const options = makeYourOptionsObject(reorganizedData);
   const chart = new CanvasJS.Chart('chartContainer', options);
   chart.render();
